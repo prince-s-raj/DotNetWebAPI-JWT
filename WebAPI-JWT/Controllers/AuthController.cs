8f713e502a3e700c2eaed7c8f69435275d22e7a8
prince-s-raj <skrajh9@gmail.com>
2022-12-10 12:21:50 +0530

    Role Based Authorization

--- WebAPI-JWT/Controllers/AuthController.cs
+++ WebAPI-JWT/Controllers/AuthController.cs
@@ -52,7 +52,8 @@ namespace WebAPI_JWT.Controllers
         {
             List<Claim> claims = new List<Claim>
             {
-                new Claim(ClaimTypes.Name, user.UserName)
+                new Claim(ClaimTypes.Name, user.UserName),
+                   new Claim(ClaimTypes.Role, "Admin")
             };
             var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
 
