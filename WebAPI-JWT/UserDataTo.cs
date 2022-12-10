67b69551d368276a5ad7257c45d59a0908fa1f02
prince-s-raj <skrajh9@gmail.com>
2022-12-10 11:29:44 +0530

    Add project files.

---
+++ WebAPI-JWT/UserDataTo.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI_JWT
+{
+    public class UserDataTo
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
